44c4134e580bbc0f0baeb884eff2a8620d05fd6c
WaiswaDonnie <57533701+WaiswaDonnie@users.noreply.github.com>
2024-09-23 02:41:43 +0300

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         hidden: { opacity: 0, y: 50 },
         visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeInOut" } },
     };
-
+ 
     return (
         <>
             <CustomNavbar />
